7624c555db5313cee2277d2c9a2d7a8414f95cea
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2021-08-26 01:50:26 +0100

    [tech: #1] Create initial svelte-kit project

---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to Svelte Club</h1>
+<p>Visit <a href="https://beta.svelte.club">beta.svelte.dev</a> to see the live version</p>
---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+		postcss: {
+			plugins: [
+				autoprefixer
+			]
+		}
+	}),
+	kit: {
+		target: '#svelte'
+	}
+};
+
+export default config;
